59eca56ff8ff617fbb7d5bcb116e89ab2e74ab00
nandidwaj <nandhu6256@gmail.com>
2026-03-30 14:46:26 +0530

    Setup and Requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''this fun will return list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Nandidwaj',
+    author_email='nandhu6256@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
